d53275d4bf60525df6b73ccbc9c7599fb8ee05b6
Erin Bond <erin.bond@code.org>
2018-01-09 16:25:40 -0800

    initial work on metrics logging

---
+++ lib/cdo/metrics_helper.rb
@@ -0,0 +1,15 @@
+require_relative './db'
+
+module Metrics
+  DEVINTERNAL_DB = sequel_connect(CDO.devinternal_db_writer, CDO.devinternal_db_writer)
+
+  # Insert data into the metrics table.
+  def self.write_metric(name, metadata, value, timestamp=nil)
+    dataset = DEVINTERNAL_DB[:metrics]
+    data = {name: name, metadata: metadata, value: value}
+    data[:created_at] = timestamp if timestamp
+    dataset.insert(data)
+  end
+end
+
+Metrics.write_metric("test_name", "This is a metric!", 0)
